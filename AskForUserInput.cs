f78cfccaa89f96c6c4704d382441fe017058854c
Candace Williams <candace.williams94@hotmail.com>
2021-08-24 14:56:43 -0400

    add developer notes on the new class created.

--- AskForUserInput.cs
+++ AskForUserInput.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace Week_5_Assignment_Test_Environment
 {
     public class AskForUserInput
-    {
+    {//this is a class. classes help to help keep code clean and organized but also help with mutiple executions.
+     //will learn more about classes. 
         public int input1()
         {
             Console.WriteLine("Insert first number:");
